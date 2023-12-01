d3a73d1756399db1883c4e7bd9cd6ed03afc9c3b
Geunbaek <rmsqor103@naver.com>
2023-12-01 16:37:10 +0900

    feat: rollup build file

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import typescript from "rollup-plugin-typescript2";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { uglify } from "rollup-plugin-uglify";
+import json from "@rollup/plugin-json";
+
+const extensions = [".js", ".ts"];
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [{ file: "dist/index.js", format: "esm" }],
+    plugins: [
+      commonjs(),
+      typescript({
+        clean: true,
+        sourceMap: false,
+      }),
+      resolve({ extensions }),
+      uglify(),
+      json(),
+    ],
+  },
+];
